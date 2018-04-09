d8fb26a9745978f9cc2773026eb1c520648c542f
Travis CI User <travis@example.org>
2018-04-09 08:50:44 +0000

    Automatic Travis documentation build

---
+++ master/implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ssd1306"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"ssd1306/builder/struct.Builder.html\" title=\"struct ssd1306::builder::Builder\">Builder</a>",synthetic:false,types:["ssd1306::builder::Builder"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
