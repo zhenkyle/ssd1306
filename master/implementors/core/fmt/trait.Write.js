4ce9bdbbe0bcf7a1cb1175080547e5de716194bd
Travis CI User <travis@example.org>
2018-04-13 08:21:08 +0000

    Automatic Travis documentation build

---
+++ master/implementors/core/fmt/trait.Write.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ssd1306"] = [{text:"impl&lt;DI&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"ssd1306/mode/terminal/struct.TerminalMode.html\" title=\"struct ssd1306::mode::terminal::TerminalMode\">TerminalMode</a>&lt;DI&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;DI: <a class=\"trait\" href=\"ssd1306/interface/trait.DisplayInterface.html\" title=\"trait ssd1306::interface::DisplayInterface\">DisplayInterface</a>,&nbsp;</span>",synthetic:false,types:["ssd1306::mode::terminal::TerminalMode"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ master/ssd1306/mode/terminal/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["TerminalMode","Handling structure for character mode display"]],"trait":[["CharacterBitmap","A trait representing a conversion from a character to a 8x8 bitmap"]]});
